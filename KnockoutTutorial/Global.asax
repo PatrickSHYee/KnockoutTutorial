e7eb26f8a49b3095e97ef8b287a4da56f2e9c32e
Patrick Yee <patrickshyee@hotmail.com>
2015-05-11 14:05:17 -0600

    Using the first tutorial to display the first name and last name bindings.

---
+++ KnockoutTutorial/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KnockoutTutorial.MvcApplication" Language="C#" %>
---
+++ KnockoutTutorial/Scripts/KOscripts/myKO.js
@@ -0,0 +1,8 @@
+﻿/* Knockout.js tutorial */
+function AppViewModel() {
+    this.fName = "Patrick";
+    this.lName = "Yee";
+}
+
+// Activate knockout.js
+ko.applyBindings(new AppViewModel());
